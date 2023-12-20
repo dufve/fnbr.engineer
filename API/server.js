d848802cf00f7c35f3524d52c51c00ebe5c37658
dufve <edvin@dufvenmark.net>
2023-12-20 22:01:12 +0100

    Add static file serving and dynamic route handler

--- API/server.js
+++ API/server.js
@@ -3,27 +3,30 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
-const security = require('./security'); // Import the security.js file
+const security = require('./security');
 
 const xalRoutes = require('./routes/xal');
-const checksumRoutes = require('./routes/checksum'); // Import the checksum routes
+const checksumRoutes = require('./routes/checksum');
 
 const app = express();
 
-// Use the security measures as middleware
 app.use(security);
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the fnbr.engineer API. This service is designed for API interactions, not for direct browser access. Please send requests to a valid endpoint to interact with the API.' });
+// Serve static files from the Website directory
+app.use(express.static(path.join(__dirname, '../Website')));
+
+// Dynamic route handler for static files
+app.get('/:page', (req, res) => {
+    res.sendFile(path.join(__dirname, `../Website/${req.params.page}/${req.params.page}.html`));
 });
 
 app.use('/xal', xalRoutes);
-app.use('/checksum', checksumRoutes); // Use the checksum routes
+app.use('/checksum', checksumRoutes);
 
-// Catch-all route handler
 app.use('*', (req, res) => {
     res.status(404).json({ message: 'Endpoint not found, you might be using the wrong method.' });
 });
